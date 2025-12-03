09c7629ff3530bf16a0cd4142fbaab3cc1010227
Vini Tran <Vinitran124@gmail.com>
2025-12-03 15:47:52 +0700

    chore: update version to 0.2.0

--- internal/version/version.go
+++ internal/version/version.go
@@ -4,7 +4,7 @@ import "time"
 
 // Current is the version string of this CLI.
 // Bump this value whenever you cut a new release.
-const Current = "0.1.3"
+const Current = "0.2.0"
 
 // LatestURL points to the VERSION file on GitHub
 // that the CLI uses to check for newer releases.
